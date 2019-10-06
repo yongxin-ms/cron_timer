316e639537e512d23f8143dfa9318009d75db088
Will <will@i007.cc>
2019-10-06 22:36:56 +0800

    增加了使用lamda表达式的例子

--- example/main.cpp
+++ example/main.cpp
@@ -5,18 +5,19 @@ void on_1_second_timer()
 	printf("1 second timer hit\n");
 }
 
-void on_3_second_timer()
-{
-	printf("3 second timer hit\n");
-}
-
 CronTimer::TimerMgr g_timer_mgr;
 
 int main()
 {
 	g_timer_mgr.Start();
+	
+	//使用全局函数的例子
 	g_timer_mgr.AddTimer("* * * * * * *", on_1_second_timer);
-	g_timer_mgr.AddTimer("0/3 * * * * * *", on_3_second_timer);
+	
+	//使用lamda表达式的例子
+	g_timer_mgr.AddTimer("0/3 * * * * * *", [](void){
+		printf("3 second timer hit\n");
+	});
 
 	while (true)
 	{
