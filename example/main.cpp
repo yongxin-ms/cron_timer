69cfc0de47850908594283140aaeff418b56eb6c
will <will@i007.cc>
2019-10-06 23:00:33 +0800

    文件编码改成了utf8-bom，vs下的启动项目优化

--- example/main.cpp
+++ example/main.cpp
@@ -1,16 +1,14 @@
 ﻿#include "../include/cron_timer_mgr.h"
 
-void on_1_second_timer()
-{
+void on_1_second_timer() {
 	printf("1 second timer hit\n");
 }
 
 CronTimer::TimerMgr g_timer_mgr;
 
-int main()
-{
+int main() {
 	g_timer_mgr.Start();
-	
+
 	//使用全局函数的例子
 	g_timer_mgr.AddTimer("* * * * * * *", on_1_second_timer);
 	
@@ -19,8 +17,7 @@ int main()
 		printf("3 second timer hit\n");
 	});
 
-	while (true)
-	{
+	while (true) {
 		g_timer_mgr.Update();
 		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
