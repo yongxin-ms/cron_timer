0c482fa3e9a43643164438245b2bdf34eaf96c0f
xinyong <xinyong@shandagames.com>
2019-10-08 17:17:47 +0800

    完善了例子

--- example/main.cpp
+++ example/main.cpp
@@ -1,28 +1,48 @@
-﻿#include "../include/cron_timer_mgr.h"
-
-void on_1_second_timer() {
-	printf("1 second timer hit\n");
-}
-
-CronTimer::TimerMgr g_timer_mgr;
+﻿#include <stdio.h>
+#include <memory>
+#include "../include/cron_timer_mgr.h"
 
 int main() {
-	g_timer_mgr.Start();
+	std::shared_ptr<CronTimer::TimerMgr> cron_timer_mgr = std::make_shared<CronTimer::TimerMgr>();
+	cron_timer_mgr->Start();
 
-	//使用全局函数的例子
-	g_timer_mgr.AddTimer("* * * * * * *", on_1_second_timer);
+	//1秒钟执行一次的定时器
+	auto timer1 = cron_timer_mgr->AddTimer("* * * * * * *", [](void) {
+		printf("1 second timer hit\n");
+	});
 	
-	//使用lamda表达式的例子
-	g_timer_mgr.AddTimer("0/3 * * * * * *", [](void){
+	//从0秒开始，每3秒钟执行一次的定时器
+	auto timer2 = cron_timer_mgr->AddTimer("0/3 * * * * * *", [](void){
 		printf("3 second timer hit\n");
 	});
 
+	//1分钟执行一次（每次都在0秒的时候执行）的定时器
+	auto timer3 = cron_timer_mgr->AddTimer("0 * * * * * *", [](void) {
+		printf("1 minute timer hit\n");
+	});
+
+	//指定时间点执行的定时器
+	auto timer4 = cron_timer_mgr->AddTimer("15;30;50 * * * * * *", [](void) {
+		printf("timer hit at 15s or 30s or 50s\n");
+	});
+
+	//指定时间段执行的定时器
+	auto timer5 = cron_timer_mgr->AddTimer("30-34 * * * * * *", [](void) {
+		printf("timer hit at 30s, 31s, 32s, 33s, 34s\n");
+	});
+
 	while (true) {
-		g_timer_mgr.Update();
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		cron_timer_mgr->Update();
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
 
-	g_timer_mgr.Stop();
+	cron_timer_mgr->RemoveTimer(timer1);
+	cron_timer_mgr->RemoveTimer(timer2);
+	cron_timer_mgr->RemoveTimer(timer3);
+	cron_timer_mgr->RemoveTimer(timer4);
+	cron_timer_mgr->RemoveTimer(timer5);
+
+	cron_timer_mgr->Stop();
     return 0;
 }
 
